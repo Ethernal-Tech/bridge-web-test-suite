c84e65574b980a915d781116c172af5e735e5dbb
Branislav Kojic <branislav.kojic@ethernal.tech>
2024-10-25 11:37:57 +0200

    Minor fixes

--- dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /opt/google-chrome.deb
-sudo apt install /opt/google-chrome.deb
-sudo apt --fix-broken install
-pip install -r requirements.txt
