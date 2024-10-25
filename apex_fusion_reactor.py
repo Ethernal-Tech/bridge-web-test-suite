47838f05d90f51b64f433aca7cd276d289b3036c
Branislav Kojic <branislav.kojic@ethernal.tech>
2024-10-25 12:48:20 +0200

    MetaMask fixes

--- apex_fusion_reactor.py
+++ apex_fusion_reactor.py
@@ -47,7 +47,7 @@ class ApexFusionReactor:
             '//*[@id="app"]/div/div[2]/main/div/div[2]/div/div/form/div[2]/button'
         ).click()
 
-        print(f"'{datetime.now()}' Receiver Address '{receiver_address}' has been funded")
+        print(f'Receiver Address {receiver_address} has been funded')
 
     @retry()
     def __open_reactor(self, source: str, destination: str) -> None:
@@ -194,7 +194,7 @@ class ApexFusionReactor:
 
     @staticmethod
     def __not_possible_bridging(source: str, destination: str) -> None:
-        print(f"'{datetime.now()}' Error: For source '{source}' destination can't be '{destination}'")
+        print(f"Error: For source '{source}' destination can't be '{destination}'")
 
     def bridging(self, source: str, destination: str, amount: str) -> None:
         if source == ApexFusionChain.prime:
@@ -237,19 +237,16 @@ class ApexFusionReactor:
         self.__connect_wallet_and_move_funds()
 
         if not self.__eternl_granted_access and type(source_wallet) == Eternl:
-            print(f"'{datetime.now()}' Eternl Wallet is waiting for access to '{self.__reactor_url}' to be granted")
+            print(f'Eternl Wallet is waiting for access to {self.__reactor_url} to be granted')
             source_wallet.grant_access()
             self.__eternl_granted_access = True
-            print(f"'{datetime.now()}' Access granted successfully")
+            print('Access granted successfully')
 
         if not self.__metamask_granted_access and type(source_wallet) == MetaMask:
-            print(f"'{datetime.now()}' MetaMask Wallet is waiting for access to '{self.__reactor_url}' to be granted")
-            try:
-                source_wallet.grant_access()
-            except IndexError:
-                pass
+            print(f'MetaMask Wallet is waiting for access to {self.__reactor_url} to be granted')
+            source_wallet.grant_access()
             self.__metamask_granted_access = True
-            print(f"'{datetime.now()}' Access granted successfully")
+            print('Access granted successfully')
 
         self.__connect_wallet_and_move_funds()
 
@@ -266,19 +263,19 @@ class ApexFusionReactor:
         else:
             self.__confirm_transaction()
 
-        print(f"'{datetime.now()}' Starting bridging from '{source}' to '{destination}' '{amount}' token(s)")
+        print(f"Starting bridging from '{source}' to '{destination}' {amount} token(s)")
 
         is_source_succeeded = self.__progress_source()
-        print(f"'{datetime.now()}' Source succeeded: '{is_source_succeeded}'")
+        print(f'Source succeeded: {is_source_succeeded}')
 
         is_bridge_succeeded = self.__progress_bridge()
-        print(f"'{datetime.now()}' Bridge succeeded: '{is_bridge_succeeded}'")
+        print(f'Bridge succeeded: {is_bridge_succeeded}')
 
         is_destination_succeeded = self.__progress_destination()
-        print(f"'{datetime.now()}' Destination succeeded: '{is_destination_succeeded}'")
+        print(f'Destination succeeded: {is_destination_succeeded}')
 
         status = self.__get_status()
-        print(f"'{datetime.now()}' Bridging status: '{status}'")
+        print(f'Bridging status: {status}')
 
         self.__disconnect_wallet()
 
